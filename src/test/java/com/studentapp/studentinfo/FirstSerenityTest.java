683c05d959e9a10e366f4fb5c976d46bbce66106
prmc924 <prcm924@gmail.com>
2024-12-28 17:30:28 +0000

    Today's Class Update

--- src/test/java/com/studentapp/studentinfo/FirstSerenityTest.java
+++ src/test/java/com/studentapp/studentinfo/FirstSerenityTest.java
@@ -1,10 +1,26 @@
 package com.studentapp.studentinfo;
 
+import com.studentapp.constants.EndPoints;
 import com.studentapp.testbase.TestBase;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.serenitybdd.rest.SerenityRest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * Created by Jay
  */
+@RunWith(SerenityRunner.class)
 public class FirstSerenityTest extends TestBase {
+
+    @Test
+    public void getAllStudents(){
+        SerenityRest.given()
+                .when()
+                .get(EndPoints.GET_ALL_STUDENT)
+                .then()
+                .log().all()
+                .statusCode(200);
+    }
 
 }
